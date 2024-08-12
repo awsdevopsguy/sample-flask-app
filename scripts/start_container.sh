3e1b0090e307af7768e0c3396d873da2814a74ed
root <root@ip-172-31-92-168.ec2.internal>
2024-08-12 10:56:22 +0000

    updated scripts files

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
 
-docker run -it -d -p 5000:5000 awsdevopsguy/sample-flask-app:v2
+# Define the container name and image
+CONTAINER_NAME="my-flask-app"
+IMAGE_NAME="awsdevopsguy/sample-flask-app:v2"
+
+# Pull the latest Docker image
+docker pull $IMAGE_NAME
+
+# Stop and remove any existing container with the same name
+if docker ps -q --filter "name=$CONTAINER_NAME" > /dev/null; then
+    echo "Stopping and removing existing container..."
+    docker stop $CONTAINER_NAME
+    docker rm $CONTAINER_NAME
+fi
+
+# Run a new container with the latest image
+echo "Starting new container with the latest image..."
+docker run -d --name $CONTAINER_NAME -p 5000:5000 $IMAGE_NAME
 
--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
-# Get the container ID of the running container
-container_id=$(docker ps -q)
-#
+# Define the container name
+CONTAINER_NAME="my-flask-app"
+
+# Get the container ID of the running container with the specified name
+container_id=$(docker ps -q --filter "name=$CONTAINER_NAME")
+
 # Check if a container ID was found
 if [ -n "$container_id" ]; then
-# Stop the Docker container
-   docker stop "$container_id"
+    echo "Stopping and removing container..."
+    docker stop "$container_id"
+    docker rm "$container_id"
 else
-   echo "No running containers found."
+    echo "No running container with name $CONTAINER_NAME found."
 fi
 
