9ab4b2a23d55d61ac7ff17c70b5838748a4551a6
root <root@ip-172-31-92-168.ec2.internal>
2024-08-12 09:15:28 +0000

    updated code with appspec file

---
+++ scripts/start_container.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+docker run -it -d -p 5000:5000 awsdevopsguy/sample-flask-app:v2
+
---
+++ scripts/stop_container.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Get the container ID of the running container
+container_id=$(docker ps -q)
+#
+# Check if a container ID was found
+if [ -n "$container_id" ]; then
+# Stop the Docker container
+   docker stop "$container_id"
+else
+   echo "No running containers found."
+fi
+
