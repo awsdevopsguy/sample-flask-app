16c86a650f570ac05001dc5e9cf1b4b03ac4f22a
awsdevopsguy <vijay.devopsprofessional@gmail.com>
2024-08-12 15:57:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 @app.route('/')
 # ‘/’ URL is bound with hello_world() function.
 def hello_world():
-    return 'Hello World deployed using AWS Code-Deploy Service!!!'
+    return 'Hello World deployed using AWS Code-Deploy Service with CI-CD Integration'
 
 # main driver function
 if __name__ == '__main__':
